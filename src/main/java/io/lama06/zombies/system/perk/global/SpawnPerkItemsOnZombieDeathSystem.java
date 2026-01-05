f165640de189a94c5eb6675ce8181f4a088d2f52
noxaa <cloveroute.com@gmail.com>
2026-01-04 20:46:28 -0500

    Add keep time to buffs

--- src/main/java/io/lama06/zombies/system/perk/global/SpawnPerkItemsOnZombieDeathSystem.java
+++ src/main/java/io/lama06/zombies/system/perk/global/SpawnPerkItemsOnZombieDeathSystem.java
@@ -15,7 +15,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.random.RandomGenerator;
 
 public final class SpawnPerkItemsOnZombieDeathSystem implements Listener {
-    private static final int PERK_ITEM_TIME = 10 * 20;
+    private static final int PERK_ITEM_TIME = 60 * 20;  // 60秒
     private static final double PERK_PROBABILITY = 0.05;
 
     @EventHandler
