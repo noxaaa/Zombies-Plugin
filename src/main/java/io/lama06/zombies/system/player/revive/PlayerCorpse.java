c5289e0c26f54f60ab6b0aebc3f207b51d2863ca
noxaa <cloveroute.com@gmail.com>
2026-02-18 02:15:42 -0500

    changed revive radius to 0.5

--- src/main/java/io/lama06/zombies/system/player/revive/PlayerCorpse.java
+++ src/main/java/io/lama06/zombies/system/player/revive/PlayerCorpse.java
@@ -4,5 +4,5 @@ public final class PlayerCorpse {
     public static final int TIME = 25 * 20;          // 25 seconds total window
     public static final int REVIVE_TIME = 40;        // 2.0 seconds normal revive
     public static final int FAST_REVIVE_TIME = 10;   // 0.5 seconds with perk
-    public static final double REVIVE_RADIUS = 4.0;  // Revive range in blocks
+    public static final double REVIVE_RADIUS = 0.5;  // Revive range in blocks
 }
